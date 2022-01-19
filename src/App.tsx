45c10f2d01405c91082b68181ee593ecf0623412
Shawn Lewis <shlewis@gmail.com>
2022-01-19 01:06:26 -0800

    Update page title

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ function App() {
         variant="success"
       />
       <div className="flex w-80 mx-auto items-center mb-8">
-        <h1 className="text-xl grow font-bold">Not Wordle</h1>
+        <h1 className="text-xl grow font-bold">Wordle 6</h1>
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer"
           onClick={() => setIsInfoModalOpen(true)}
